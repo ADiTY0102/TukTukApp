75472e6cd4cc1703887b9b1d348ae435a085a732
ADiTY0102 <adityabinjagermath12@gmail.com>
2025-12-07 16:15:16 +0530

    FinalStage--update/Create-- 4:15 pm

--- frontend/src/components/FinishedRide.jsx
+++ frontend/src/components/FinishedRide.jsx
@@ -1,30 +1,23 @@
-import React, { useState } from "react";
+import React from 'react';
 import { Link } from "react-router-dom";
-const ConfirmRidePopUp = (props) => {
 
-  const [otp, setOtp] = useState('')
-  
-  const submitHandler =(e)=>{
-    e.preventDefault();
-  }
-
-
+const FinishedRide = (props) => {
   return (
-    <div>
+    <div className='w-full'>
       <h5
-        className="p-1 text-center absolute w-[95%] top-0"
+        className="p-1 text-center absolute w-[95%] top-2"
         onClick={() => {
-          props.setConfirmRidePopUp(false);
+          props.setFinishRidePanel(false);
         }}
       >
         <i className="text-3xl text-gray-400 ri-arrow-down-wide-line"></i>
       </h5>
-      <h3 className="text-sm font-semibold bg-gray-200 rounded-full w-35 pl-2 p-1 mb-1.5">
-        Confirm Ride<i className="ri-arrow-right-s-line"></i>{" "}
+      <h3 className="text-lg font-semibold pl-0 p-1 mb-1.5">
+        Finish This Ride<i className="ri-arrow-right-s-line"></i>{" "}
       </h3>
 
-      <div className="flex items-center justify-between bg-yellow-300 rounded-lg p-3 mt-3">
-        <div className="flex items-center gap-3">
+      <div className="flex items-center justify-between mt-4 bg-yellow-300 rounded-lg p-3">
+        <div className="flex items-center">
           <img
             className="h-12 w-12 rounded-full object-cover"
             src="https://img.freepik.com/free-photo/indian-woman-posing-cute-stylish-outfit-camera-smiling_482257-122351.jpg?semt=ais_hybrid&w=740&q=80"
@@ -64,33 +57,16 @@ const ConfirmRidePopUp = (props) => {
           </div>
         </div>
         <div className="mt-5">
-          <form onSubmit={(e)=>{
-            submitHandler(e);
-          }}>
-            <input value={otp} onChange={(e)=>setOtp(e.target.value)} className="bg-[#eee] px-10 font-mono py-2 text-lg rounded-lg w-full mb-3" type="text" placeholder="Enter OTP" />
-            <Link
+          <Link
               to={"/captain/riding"}
-              onClick={() => {
-                props.setConfirmRidePopUp(false);
-              }}
               className="w-full flex justify-center text-white font-semibold bg-green-600 rounded-lg p-2"
             >
-              Confirm
+              Complete Ride
             </Link>
-            <button
-              onClick={() => {
-                props.setRidePopUp(false);
-                props.setConfirmRidePopUp(false);
-              }}
-              className="w-full text-gray-700 font-semibold bg-gray-200 rounded-lg p-2"
-            >
-              Cancel
-            </button>
-          </form>
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default ConfirmRidePopUp;
+export default FinishedRide;
--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -1,6 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import {React,useState,useRef} from "react";
+import { Link } from "react-router-dom";
+import FinishedRide from "../components/FinishedRide";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const CaptainRiding = () => {
+
+  const [finishRidePanel, setFinishRidePanel] = useState(false);
+
+  const finishRidePanelRef = useRef(null);
+
+
+
+  useGSAP(
+    function () {
+      if (finishRidePanel) {
+        gsap.to(finishRidePanelRef.current, {
+          y: 0,
+          duration: 0.4,
+          ease: "power3.out",
+        });
+      } else {
+        gsap.to(finishRidePanelRef.current, {
+          y: "100%",
+          duration: 0.4,
+          ease: "power3.in",
+        });
+      }
+    },
+    [finishRidePanel]
+  );
+
   return (
     <div className="h-screen">
       <div className=" fixed p-3 w-screen flex justify-between items-center">
@@ -14,7 +44,7 @@ const CaptainRiding = () => {
       </div>
 
       {/* map-part */}
-      <div className="h-4/5">
+      <div className="h-[85%]">
         <img
           src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
           alt="Home"
@@ -22,13 +52,33 @@ const CaptainRiding = () => {
         />
       </div>
       {/* captain-details */}
-      <div className="h-1/5 p-6">
-        
+      <div onClick={()=>{
+        setFinishRidePanel(true)
+      }} className="h-[15%] p-6 bg-yellow-400 flex items-center justify-between relative rounded-t-3xl">
+        <h5
+          className="p-1 text-center absolute w-screen top-0"
+          onClick={() => {
+            props.setConfirmRidePopUp(false);
+          }}
+        >
+          <i className="text-3xl text-gray-900 ri-arrow-up-wide-line"></i>
+        </h5>
+        <h4 className="text-lg font-semibold">4 km Away.</h4>
+        <button className="bg-black p-2 font-semibold px-10 rounded-lg text-white">
+          Complete Ride
+        </button>
       </div>
-      
-      
+      <div
+        ref={finishRidePanelRef}
+        className="fixed w-full h-screen z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12"
+      >
+        <FinishedRide
+          finishRidePanel={finishRidePanel}
+          setFinishRidePanel={setFinishRidePanel}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default CaptainRiding;
