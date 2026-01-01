91eb08803313c94bd808e83810ce1a46b0f4fc5f
ADiTY0102 <adityabinjagermath12@gmail.com>
2026-01-01 17:42:13 +0530

    schemaAnalysis-- comments--

--- frontend/src/pages/UserSignUp.jsx
+++ frontend/src/pages/UserSignUp.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate} from "react-router-dom";
 import axios from 'axios';
 import{ UserDataContext } from '../context/userContext.jsx';
 
-
+//registration
 const UserSignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
